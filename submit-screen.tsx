4bf933fe78d62a528deec02a50fe38dcdcde4b4a
folashaderoberts34-max <folashaderoberts34@gmail.com>
2026-04-13 01:26:16 -0500

    Update genre options and display text in submit-screen.tsx

---
+++ submit-screen.tsx
@@ -0,0 +1,11 @@
+// Updated genre options
+const genreOptions = [
+    'Fiction',
+    'Memoir',
+    'Poetry',
+    'Essay',
+    'Creative Nonfiction'
+];
+
+// Update display text
+const displayText = 'Creative Nonfiction';
